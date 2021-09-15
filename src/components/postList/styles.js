660cd36dc865d8d83b8137b0ade94c765d7c73a4
ldh7228 <ldh7228@gmail.com>
2021-09-15 11:54:52 +0900

    [ADD main

---
+++ src/components/postList/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 495px;
+  .postTitle > span {
+    margin-left: 54.5px;
+    margin-right: 295.5px;
+  }
+`;
