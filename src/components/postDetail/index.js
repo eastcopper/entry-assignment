8beeb666780723e4aed5809a334059ad7b042893
ldh7228 <ldh7228@gmail.com>
2021-09-15 14:32:50 +0900

    [ADD] postDetail
    게시물 상세보기 페이지

--- src/components/postDetail/index.js
+++ src/components/postDetail/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import * as S from "../postDetail/styles";
+import React from "react";
+import Header from "../Header";
 
-export default function A(){
-    return (
-        <>
-            
-        </>
-    )
+export default function PostDetail() {
+  let writer = true;
+
+  return (
+    <>
+      <Header />
+      <S.Main>
+        <table className="title">
+            <span>엔트리 프론트엔드 인턴 게시판입니다.</span>
+            <span>작성자</span>
+        </table>
+        <table className="content">
+          <br />
+          <span>엔트리 프론트엔드 인턴 게시판 상세보기 디자인입니다.</span>
+        </table>
+        <div className="fix">
+          {writer ? <button className="fixbtn">수정하기</button> : <span> </span>}  
+        </div>
+      </S.Main>
+    </>
+  );
 }
