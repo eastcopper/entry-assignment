c097edac70229cab83f61c5d8597871dfde6888c
ldh7228 <ldh7228@gmail.com>
2021-09-17 10:49:56 +0900

    [ADD] route
    라우트 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import PostDetail from "./components/postDetail";
 import PostList from "./components/postList";
 import PostWrite from "./components/postWrite";
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <>
-      <PostWrite/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <PostList />
+          </Route>
+          <Route path="/detail">
+            <PostDetail />
+          </Route>
+          <Route path="/write">
+            <PostWrite />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
