e3b96f5d66d045bef16bbd75104c003cef2a36db
ldh7228 <ldh7228@gmail.com>
2021-09-17 22:24:12 +0900

    [FIX] app
    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import PostDetail from "./components/postDetail/Detail";
-import PostList from "./components/postList/List";
-import PostWrite from "./components/postWrite/Write";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import Container from "./container/container";
+import Header from "./components/Header/Header";
+import Input from "./components/postWrite/postWrite";
 import { Component } from "react";
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: `http://3.36.82.24:8080`
-})
 
 export default class App extends Component {
-
-  constructor() {
-    super();
-    api.get('/').then(res => {
-      console.log(res.data)
-    })
-  }
-
-  render() {
+  render () {
     return (
       <>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/">
-              <PostList/>
-            </Route>
-            <Route path="/detail">
-              <PostDetail />
-            </Route>
-            <Route path="/write">
-              <PostWrite />
-            </Route>
-          </Switch>
-        </BrowserRouter>
+        <Container />
       </>
     );
   }
 }
+
