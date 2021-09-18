439ac53dc24226c7275b3cbc112f852d8f11a01c
ldh7228 <ldh7228@gmail.com>
2021-09-18 14:36:12 +0900

    [ADD] page move
    페이지 이동 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,14 @@ export default function Header() {
   return (
     <>
       <S.Header>
-        <span className="post">게시물 보기</span>
-        <Link to="/write"className="post">게시물 등록</Link>
+        <span className="post">
+          <Link to="/" className="post">
+            게시물 보기
+          </Link>
+        </span>
+        <Link to="/write" className="post">
+          게시물 등록
+        </Link>
         <button className="login">로그인</button>
       </S.Header>
     </>
--- src/components/postList/postList.js
+++ src/components/postList/postList.js
@@ -1,19 +1,23 @@
 import React from "react";
 import * as S from "./styles";
+import { Link } from "react-router-dom";
 
 export default function PostList({
   boardId,
   boardTitle,
   boardContent,
   onRemove,
-  onRowClick,
 }) {
   return (
     <>
       <S.Tr>
-        <td className="numbers" onClick={() => onRowClick(boardId)}>{boardId}</td>
-        <td className="title" onClick={() => onRowClick(boardId)}>{boardTitle}</td>
-        <td className="contents" onClick={() => onRowClick(boardId)}>{boardContent}</td>
+        <td className="numbers">
+          <Link to={`/detail/${boardId}`}>{boardId}</Link>
+        </td>
+        <td className="title">
+          <Link to={`/detail/${boardId}`}>{boardTitle}</Link>
+        </td>
+        <td className="contents">{boardContent}</td>
         <td>
           <button onClick={() => onRemove(boardId)}>삭제</button>
         </td>
